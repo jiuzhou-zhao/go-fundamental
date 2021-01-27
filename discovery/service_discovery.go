401b3aff1ffc7d510b58d0f7c66849c141a51121
zhaojiuzhou <zhaojiuzhou@ymipro.com>
2021-01-27 09:13:51 +0800

    update discovery

--- discovery/service_discovery.go
+++ discovery/service_discovery.go
@@ -1,10 +1,40 @@
 package discovery
 
-import "github.com/jiuzhou-zhao/go-fundamental/clienttoolset"
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	TypeGRpc = "grpc"
+	TypeHttp = "http"
+)
 
 type ServiceInfo struct {
-	Name          string
-	GRpcAddresses *clienttoolset.GRpcClientConfig
+	Host        string
+	Port        int
+	ServiceName string // type:name:index
+	Meta        map[string]string
 }
 
-type Observer func(services map[string][]*ServiceInfo)
+type Observer func(services map[string]*ServiceInfo)
+
+func BuildServerName(t string, name string, index string) string {
+	t = strings.ToLower(t)
+	return fmt.Sprintf("%v:%v:%v", t, name, index)
+}
+
+func ParseServerName(n string) (t string, name string, index string, err error) {
+	vs := strings.Split(n, ":")
+	if len(vs) < 2 || len(vs) > 3 {
+		err = fmt.Errorf("invalid server name: %v", n)
+		return
+	}
+	t = vs[0]
+	t = strings.ToLower(t)
+	name = vs[1]
+	if len(vs) == 3 {
+		index = vs[2]
+	}
+	return
+}
